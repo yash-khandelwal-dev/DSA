c637e5cf8587d881c04494c1982cd16d7fd9eece
Yash khandelwal <yashkhandelwal0409@gmail.com>
2025-11-22 20:36:32 +0530

    Improved Search Insert Position solution using binary search (O(log n))

--- arrays/SearchInteger.java
+++ arrays/SearchInteger.java
@@ -7,17 +7,25 @@ package arrays;
  * Difficulty: Easy
  * Author: Yash Khandelwal
  * Language: Java
- * Time Complexity: O(n)
+ * Time Complexity: O(log(n))
  */
 
+// Binary Search Approach(Most Optimal)
 
 public class SearchInteger {
     public int searchInsert(int[] nums, int target) {
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] >= target){
-                return i;
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] >= target){
+                high = mid - 1;
             }
+            else 
+                low = mid + 1;
         }
-        return nums.length;
+        return low;
     }
 }
