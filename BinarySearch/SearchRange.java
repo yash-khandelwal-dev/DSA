dd398c7264e0900f11c161501560460fa979ae86
Yash khandelwal <yashkhandelwal0409@gmail.com>
2025-11-22 20:48:46 +0530

    Added binary search solution for Search Range (O(log n))

---
+++ BinarySearch/SearchRange.java
@@ -0,0 +1,51 @@
+package binarysearch;
+
+/*
+ * Problem: Search for a Range
+ * Problem link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+ * Platform: LeetCode
+ * Difficulty: Medium
+ * Author: Yash Khandelwal
+ * Language: Java
+ * Time Complexity: O(log(n))
+ */
+
+
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        int before = -1;
+        int after = -1;
+        int mid;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] == target ){
+                before = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] > target)
+                high = mid - 1;
+                else 
+                    low = mid + 1;
+        }
+        if(before == -1){
+            return new int[] {-1,-1};
+        }
+        low = 0;
+        high = nums.length - 1;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] == target){
+                after = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] > target)
+                high = mid - 1;
+                else 
+                    low = mid + 1;
+        }
+            return new int[] {before,after};
+    }
+}
+}
